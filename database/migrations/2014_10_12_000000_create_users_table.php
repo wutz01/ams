c6817a3bdfa8409152ab40de58d75bc579054b8c
John Perez <john.perez@kodakit.com>
2018-03-12 11:26:09 +0800

    fix(ui-tweaks): updated migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,27 +16,78 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->string('password')->nullable();
             $table->string('firstname')->nullable();
             $table->string('middlename')->nullable();
             $table->string('lastname')->nullable();
-            $table->text('address')->nullable();
-            $table->string('city')->nullable();
-            $table->string('zipCode')->nullable();
-            $table->string('mobileNo')->nullable();
-            $table->string('phoneNo')->nullable();
-            $table->string('country')->nullable();
-            $table->string('companyName')->nullable();
-            $table->string('companyEmail')->nullable();
-            $table->string('lineBusiness')->nullable();
-            $table->string('companyAddress')->nullable();
-            $table->string('companyCity')->nullable();
-            $table->string('companyZipCode')->nullable();
-            $table->string('companyLandLine')->nullable();
-            $table->string('companyCountry')->nullable();
-            $table->string('designation')->nullable(); // Type of Company: Government / Private
             $table->string('userType'); // ADMIN / CLIENT / SALES_AGENT
             $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('masterlist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('churchId')->unique();
+            $table->string('firstname')->nullable();
+            $table->string('middlename')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('lokalOrigin')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('sabbathDay')->nullable();
+            $table->string('contactNumber')->nullable();
+            $table->binary('fingerPrint')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('workers_masterlist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('churchId')->unique();
+            $table->string('firstname')->nullable();
+            $table->string('middlename')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('lokalOrigin')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('sabbathDay')->nullable();
+            $table->string('contactNumber')->nullable();
+            $table->binary('fingerPrint')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('groupings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('area')->nullable();
+            $table->text('description')->nullable();
+            $table->string('type')->nullable()->default('LG'); // LG (lokal group) / Committee
+            $table->timestamps();
+        });
+
+        Schema::create('groupings_masterlist', function (Blueprint $table) {
+          $table->integer('group_id')->unsigned()->nullable();
+          $table->foreign('group_id')->references('id')
+            ->on('groupings')->onDelete('cascade');
+          $table->integer('brethren_id')->unsigned()->nullable();
+          $table->foreign('brethren_id')->references('id')
+            ->on('masterlist')->onDelete('cascade');
+        });
+
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('batch')->comment('WS/PM/TG');
+            $table->string('time'); // (7:30AM.. etc)
+            $table->string('date'); // (02/11/2018.. etc)
+            $table->integer('worker_assign')->unsigned(); // from workers_masterlist
+            $table->integer('officer_assign')->unsigned(); // from masterlist
+            $table->timestamps();
+        });
+
+        Schema::create('attendance_list', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('attendance_id')->unsigned(); // from workers_masterlist
+            $table->integer('brethren_id')->unsigned(); // from masterlist
             $table->timestamps();
         });
     }
@@ -48,6 +99,11 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('attendance_list');
+        Schema::dropIfExists('attendance');
+        Schema::dropIfExists('groupings_masterlist');
+        Schema::dropIfExists('groupings');
+        Schema::dropIfExists('masterlist');
         Schema::dropIfExists('users');
     }
 }
