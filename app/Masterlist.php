ed8fbc43a680e7568f762b773cc4b2161b8e164e
John Perez <john.perez@kodakit.com>
2018-03-28 11:11:25 +0800

    fix(ui-tweaks): hide fingerprint

--- app/Masterlist.php
+++ app/Masterlist.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Masterlist extends Model
 {
     protected $table = 'masterlist';
+    protected $hidden = ['fingerPrint'];
 
     public function image () {
       return $this->hasOne('App\UserImage', 'userId');
